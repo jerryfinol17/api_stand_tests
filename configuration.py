62b5fe45ad75fc25bc004167850d4df295773965
Jerry Dhamyan Finol Sanchez <Jerrytareas17@gmail.com>
2025-08-02 14:35:38 -0400

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE= 'https://cnt-e178e912-1f4e-4b1f-ba01-e3d62b65183c.containerhub.tripleten-services.com'
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = '/api/logs/main/'
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
